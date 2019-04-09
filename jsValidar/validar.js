385744270fa5c4f77e551a54e0eb23ce7f11573a
elvira roca <c1erocav@ieslavereda.es>
2019-04-09 23:01:21 +0200

    formulario HazteVoluntario

--- jsValidar/validar.js
+++ jsValidar/validar.js
@@ -8,6 +8,17 @@ function cambiarFondoUsuario()
     fondoUsuario.style.backgroundColor="#d1d1c6";
 }
 
+function cambiarFondoUsuarios(nombre)
+{
+    if(nombre.value.match(/^[A-Z][a-z]{1,14}$/))
+    {
+        nombre.style.backgroundImage= 'url("../imagenes/vistoVerde.png")';
+    }else
+    {
+        nombre.style="border: 1px solid red;"
+    }
+}
+
 function cambiarFondoPassword()
 {
     fondoPassword=document.getElementById("password");
@@ -26,8 +37,67 @@ function cambiarFondoDNI()
     fondoPassword.style.backgroundColor="#d1d1c6";
 }
 
+function cambiarFondoDireccion()
+{
+    fondoPassword=document.getElementById("direccion");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoNumero()
+{
+    fondoPassword=document.getElementById("numero");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoPortal()
+{
+    fondoPassword=document.getElementById("portal");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoPiso()
+{
+    fondoPassword=document.getElementById("piso");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoLetra()
+{
+    fondoPassword=document.getElementById("letra");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoLetra()
+{
+    fondoPassword=document.getElementById("letra");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoCorreo()
+{
+    fondoPassword=document.getElementById("correo");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoTelf1()
+{
+    fondoPassword=document.getElementById("telefono1");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
+
+function cambiarFondoTelf2()
+{
+    fondoPassword=document.getElementById("telefono2");
+    fondoPassword.style.backgroundColor="#d1d1c6";
+}
 
 
 
+
+
+
+
+
+cambiarFondoNumero
 
 
