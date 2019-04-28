dd8b40b5a4e6fa5472f1c0f597f486bc12415860
elvira roca <c1erocav@ieslavereda.es>
2019-04-28 13:31:55 +0200

    Con la pagina index

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include("views/partials/cabecera.part.php");
                         <div class="carousel-caption d-block">
                                 <p class="textoDentroFoto">Gatos en adopción</p>
                         </div>
-                        <img class="fotoGato" src="gatosAdopcion/03.jpg">
+                        <img class="fotoGato" src="gatosAdopcion/05.jpg">
                     </div>
                     <div class="carousel-item">
                         <img class="fotoGato" src="gatosAdopcion/02.jpg">
@@ -29,7 +29,7 @@ include("views/partials/cabecera.part.php");
                         <img class="fotoGato" src="gatosAdopcion/04.jpg">
                     </div>
                     <div class="carousel-item">
-                        <img class="fotoGato" src="gatosAdopcion/05.jpg">
+                        <img class="fotoGato" src="gatosAdopcion/03.jpg">
                     </div>
                     <div class="carousel-item">
                         <img class="fotoGato" src="gatosAdopcion/07.jpg">
@@ -45,39 +45,60 @@ include("views/partials/cabecera.part.php");
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12 indexHazteVoluntario mt-md-5 ml-md-5">
+        <div class="col-md-12 indexHazteVoluntario mt-md-5 offset-md-1">
             <p>Hazte voluntario</p>
         </div>
-        <div class="col-md-10 indexHazteVoluntarioParraf mt-md-1 ml-md-5">
+    </div>
+    <div class="row">
+        <div class="col-md-8 indexHazteVoluntarioParraf mt-md-1 offset-md-1">
             <p>Si quieres colaborar con nosostros, puedes hacerte voluntario, lo único que tienes que hacer es rellenar
                     el formulario y así formalizar la inscripción.</p>
         </div>
     </div>
+
     <div class="row">
-        <div class="cajaBoton cajaBoton mt-md-1 mb-md-1 offset-md-9">
+        <div class="col-md-3 cajaBoton mt-md-1 offset-md-1">
             <a href="hazteVoluntario.php" class="btn btn-primary boton1" role="button">Hazte Voluntario</a>
+        </div>
+
+        <div class="col-md-5 offset-md-7">
+            <img class="fotoIndex1 " src="gatosAdopcion/indexHazteVol.jpg" alt="foto">
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-md-12 indexHazteVoluntario mt-md-5 offset-md-1">
+            <p>Ayúdanos</p>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-md-12 indexHazteVoluntario mt-md-5 ml-md-5">
-            <p>Ayúdanos</p>
-        </div>
-        <div class="col-md-10 indexHazteVoluntarioParraf mt-md-1 ml-md-5">
+        <div class="col-md-8 indexHazteVoluntarioParraf mt-md-1 offset-md-1">
             <p>Nos gustaría mucho que nos ayudases haciéndote socio, es muy fácil, únicamnete debes rellenar un formulario. Tu aportación, hará más por los animales.</p>
         </div>
     </div>
-    <div class="row">
-        <div class="cajaBoton cajaBoton mt-md-1 mb-md-1 offset-md-9">
-            <a href="hazteVoluntario.php" class="btn btn-primary boton1" role="button">Ayúdanos</a>
-        </div>
-    </div>
 
     <div class="row">
-        <div class="col-md-12 indexHazteVoluntario mt-md-5 ml-md-5">
+        <div class="col-md-3 cajaBoton mt-md-1 offset-md-8">
+            <a href="hazteVoluntario.php" class="btn btn-primary boton1" role="button">Ayúdanos</a>
+        </div>
+
+        <div class="col-md-5 offset-md-1">
+            <img class="fotoIndex2" src="gatosAdopcion/indexHazteAyu.jpg" alt="foto">
+        </div>
+
+    </div>
+
+
+
+
+    <div class="row">
+        <div class="col-md-12 indexHazteVoluntario mt-md-5 offset-md-1">
             <p>Adopta</p>
         </div>
-        <div class="col-md-10 indexHazteVoluntarioParraf mt-md-1 ml-md-5">
+        <div class="col-md-8 indexHazteVoluntarioParraf mt-md-1 offset-md-1">
             <p>Estos son algunos de los gatos que están esperando para ser adoptados.</p>
         </div>
 
